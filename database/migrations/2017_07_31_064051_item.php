cd23a49bb076f93d908c04b5ec7975f7d71883fe
ArifSuhardinata <arif_suhardinata1@yahoo.com>
2017-07-31 17:19:47 +0700

    Update 2017_07_31_064051_item.php

--- database/migrations/2017_07_31_064051_item.php
+++ database/migrations/2017_07_31_064051_item.php
@@ -13,7 +13,16 @@ class Item extends Migration
      */
     public function up()
     {
-        //
+               Schema::create('item', function (Blueprint $table) {
+            $table->increments('no_resi')->unique();
+            $table->string('nama_item');
+            $table->string('nama_penerima');
+            $table->string('nama_pengirim');
+            $table->string('alamat_pengirim');
+            $table->text('alamat_penerima');
+            $table->string('nomor_telepon_penerima');
+            $table->string('nomor_telepon_pengantar');
+            $table->integer('berat');
     }
 
     /**
